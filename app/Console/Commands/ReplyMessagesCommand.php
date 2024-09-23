5e828b90ad674b1c65413d22fed0fc0118740fd7
Akrez <akrez.like@gmail.com>
2024-09-23 21:43:41 +0330

    up++

--- app/Console/Commands/ReplyMessagesCommand.php
+++ app/Console/Commands/ReplyMessagesCommand.php
@@ -8,6 +8,7 @@ use App\Support\Chart;
 use App\Support\Sparkline;
 use App\Support\TelegramApi;
 use Illuminate\Console\Command;
+use Illuminate\Support\Number;
 
 class ReplyMessagesCommand extends Command
 {
@@ -42,9 +43,9 @@ class ReplyMessagesCommand extends Command
                 $chats = Coin::query()->where('coin_id', $coin->coin_id)->get();
                 $text = implode("\n", [
                     ($minMaxResult['is_min'] ? '🔴' : '🟢').' #'.$coin->coin_name,
-                    '⬆️ '.$minMaxResult['max_hour']['price'],
-                    ($minMaxResult['is_min'] ? '➖' : '➕').' '.($minMaxResult['max_hour']['price'] - $minMaxResult['min_hour']['price']),
-                    '⬇️ '.$minMaxResult['min_hour']['price'],
+                    '⬆️ '.Number::format($minMaxResult['max_hour']['price']),
+                    ($minMaxResult['is_min'] ? '➖' : '➕').' '.(Number::format($minMaxResult['max_hour']['price']) - $minMaxResult['min_hour']['price']),
+                    '⬇️ '.Number::format($minMaxResult['min_hour']['price']),
                     date('Y-m-d H:i:s'),
                     'https://bitpin.ir/coin/'.$coin->coin_name,
                 ]);
